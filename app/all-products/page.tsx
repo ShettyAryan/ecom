4c043d28b7d3246a6af830bfb1444293c2129d6d
Aryan Shetty <shetty.aryan1923@gmail.com>
2025-06-01 00:37:33 +0530

    All products page

---
+++ app/all-products/page.tsx
@@ -0,0 +1,30 @@
+'use client'
+import Footer from '@/components/Footer';
+import Navbar from '@/components/Navbar';
+import ProductCard from '@/components/ProductCard';
+import { useAppContext } from '@/context/AppContext'
+import React from 'react'
+
+const allProducts = () => {
+
+ const {products} = useAppContext();
+
+  return (
+    <>
+     <Navbar />
+     <div className='flex flex-col items-start px-6 md;px-16 lg:px-32'>
+        <div className='flex flex-col items-end pt-12'>
+            <p className='text-2xl font-medium'>All Products</p>
+            <div className='w-16 h-0.5 bg-orange-600 rounded-full'></div>
+        </div>
+        <div className='grid grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 flex-col items-center gap-6 mt-12 pb-14 w-full'>
+            {products.map((product,index:number)=><ProductCard key={index} product={product} />)}
+        </div>
+
+     </div>
+     <Footer />
+    </>
+  )
+}
+
+export default allProducts
