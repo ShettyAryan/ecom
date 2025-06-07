5bbba2144ca27e66766683e79f0ca180f29135ae
Aryan Shetty <shetty.aryan1923@gmail.com>
2025-06-07 22:42:51 +0530

    orders placed components

---
+++ components/Loading.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Loading = () => {
+  return (
+    <div className='felx justify-center items-center h-[70vh]'>
+        <div className='animate-spin rounded-full h-20 w-20 border-4 border-t-orange-300 border-gray-200'></div>
+
+    </div>
+  )
+}
+
+export default Loading
