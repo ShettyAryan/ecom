c90372e892238180737c3a4f6d33df84dbf877bd
Aryan Shetty <shetty.aryan1923@gmail.com>
2025-06-11 17:05:16 +0530

    authseller change

--- lib/authSeller.ts
+++ lib/authSeller.ts
@@ -1,23 +1,35 @@
 import { clerkClient } from "@clerk/nextjs/server"
 import { NextResponse } from "next/server"
 
-const authSeller = async (userId:string) => {
+const authSeller = async (userId:string|null) => {
     try{
+        if(!userId) return false;
+        
         const client = await clerkClient()
         const user = await client.users.getUser(userId)
-
-        if(user.publicMetadata.role === 'seller'){
+            if(user.publicMetadata.role === 'seller'){
             return true;
         } else{
             return false;
         }
+        
+        
+
+        
 
     }
-    catch (error:any){
-        return NextResponse.json({
+    catch (error:unknown){
+        if(error instanceof Error){
+            return NextResponse.json({
             success:false,
             message: error.message
         })
+        }else{
+            return NextResponse.json({
+            success:false,
+            message: "Something went wrong"
+        })
+        }
   
     }
  
